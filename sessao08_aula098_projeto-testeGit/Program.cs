40be9708945936551b0b589d92d726b6a633d7f9
Daniel Xavier <daniel.xds@hotmail.com>
2020-08-07 11:31:43 -0300

    Meu primeiro commit do projeto

---
+++ sessao08_aula098_projeto-testeGit/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace sessao08_aula098_projeto_testeGit {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
